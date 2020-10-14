8a5ad8464b182a0a8a861ea56e23d5d52cda16dd
ui-manufaktur <ui.manufaktur@gmail.com>
2020-10-14 07:12:48 +0200

    Init

--- source/uim/forensic/package.d
+++ source/uim/forensic/package.d
@@ -1,1 +1,13 @@
 module uim.forensic;
+
+public import std.stdio;
+public import std.string;
+public import std.uuid;
+
+// external packages
+public import vibe.d;
+
+// uim packages
+public import uim.core;
+public import uim.oop;
+
